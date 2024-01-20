a4121c91b68fa17f2fe8d1fc5e133aec759cf766
Georgy Levin <g.phyzy@gmail.com>
2024-01-20 17:31:29 +0200

    class AvgReducerAppl + interface AvgValueService

---
+++ avg-redicer/src/main/java/telran/probes/AvgReducerAppl.java
@@ -0,0 +1,46 @@
+package telran.probes;
+
+import java.util.function.Consumer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.context.annotation.Bean;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import telran.probes.dto.ProbeData;
+import telran.probes.service.AvgValueService;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
+public class AvgReducerAppl {
+final AvgValueService providerService;
+final StreamBridge streamBridge;
+@Value("${app.deviation.binding.name:deviation-out-0}")
+String deviationBindingName;
+	public static void main(String[] args) {
+		SpringApplication.run(AvgReducerAppl.class, args);
+
+	}
+	
+	@Bean
+	public Consumer<ProbeData> consumerProbeData() {
+		return this::ConsumerMethod;
+	}
+	
+	private void ConsumerMethod(ProbeData probeData) {
+		log.trace("recived probeData: {}", probeData);
+		Long avgValue = providerService.getAvgValue(probeData);
+		log.debug("avgValue: {}", avgValue);
+		if(!avgValue.equals(null)) {
+			streamBridge.send(deviationBindingName, avgValue);
+			log.debug("average value {} sent to {}", avgValue, deviationBindingName);
+		}
+		
+		
+	}
+
+}
