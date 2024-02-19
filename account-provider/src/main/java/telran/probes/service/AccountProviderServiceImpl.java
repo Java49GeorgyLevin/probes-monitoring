0616a92bb987613639f0b87ba2de3b728ca82954
Georgy Levin <g.phyzy@gmail.com>
2024-02-19 18:14:52 +0200

    NotFoundException()

--- account-provider/src/main/java/telran/probes/service/AccountProviderServiceImpl.java
+++ account-provider/src/main/java/telran/probes/service/AccountProviderServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
+import telran.exceptions.NotFoundException;
 import telran.probes.repo.AccountProviderRepo;
 import telran.security.accounting.dto.AccountDto;
 import telran.security.accounting.model.Account;
@@ -17,7 +18,7 @@ public class AccountProviderServiceImpl implements AccountProviderService {
 	@Override
 	public AccountDto getAccountDto(String email) {
 		Account account = accountRepo.findById(email)
-		.orElseThrow(() -> new IllegalStateException(String.format("account %s not found", email)));
+		.orElseThrow(() -> new NotFoundException(String.format("account %s not found", email)));
 		log.debug("account {} was found", email);
 		
 		return account.build();
