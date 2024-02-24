6588f61ce387c2500fae625fdbcd2457979610c8
Georgy Levin <g.phyzy@gmail.com>
2024-02-24 16:26:00 +0200

    fixed bug

--- gateway/src/main/java/telran/probes/configuration/SecurityConfiguration.java
+++ gateway/src/main/java/telran/probes/configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package telran.probes.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -10,8 +11,26 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.RestTemplate;
 
+import telran.probes.api.UrlConstants;
+
 @Configuration
 public class SecurityConfiguration {
+	@Value("${app.update.emails.role:EMAILS_ADMIN}")
+	String emailsAdminRole;
+	@Value("${app.update.range.role:RANGES_ADMIN}")
+	String rangesAdminRole;
+	@Value("${app.use.emails.role:EMAILS_USER}")
+	String emailsUserRole;
+	@Value("${app.use.range.role:RANGES_USER}")
+	String rangesUserRole;
+	@Value("${app.use.account.role:ACCOUNT_USER}")
+	String accountsUserRole;
+	@Value("${app.sensor.range.provider.url:/range/sensor}")
+	String rangeSensorUrl;
+	@Value("${app.emails.provider.url:/emails/sensor}")
+	String emailsSensorUrl;
+	@Value("${app.account.url:/accounts}")
+	String accountsUrl;
 @Bean
 PasswordEncoder getPasswordEncoder() {
 	return new BCryptPasswordEncoder();
@@ -24,7 +43,12 @@ RestTemplate getRestTemplate() {
 SecurityFilterChain configure(HttpSecurity http) throws Exception {
 	http.cors(custom -> custom.disable());
 	http.csrf(custom -> custom.disable());
-	http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
+	http.authorizeHttpRequests(requests -> requests
+			.requestMatchers(UrlConstants.UPDATE_EMAILS).hasRole(emailsAdminRole)
+			.requestMatchers(UrlConstants.UPDATE_RANGE).hasRole(rangesAdminRole)
+			.requestMatchers(emailsSensorUrl + "/**").hasRole(emailsUserRole)
+			.requestMatchers(rangeSensorUrl + "/**").hasRole(rangesUserRole)
+			.requestMatchers(accountsUrl + "/**").hasRole(accountsUserRole));
 	http.httpBasic(Customizer.withDefaults());
 	http.sessionManagement(custom -> custom.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
 	return http.build();
